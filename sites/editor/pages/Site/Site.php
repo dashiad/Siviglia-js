8a89f8c374f28c61a00a4c704efdd6ecec7e87fb
frodriguezsmartclip <frodriguez@smartclip.com>
2020-01-23 18:02:14 +0100

    estructura LAYAOUT  dentro de sites/editor/widgets

--- sites/editor/pages/Site/Site.php
+++ sites/editor/pages/Site/Site.php
@@ -1,17 +1,13 @@
-[*HTML/HTMLPAGE]
+[*PAGE/SITE]
     [_TITLE]EDITOR SECCION - v.1.0 beta Smartclip[#]
 
     [_CONTENT]
-        [*:LAYOUTS/EDITOR_LAYOUT({"useSidebar":"$sidebar"})]
-            [_:CONTENT]
-
-                [*::COMPONENTS/CARD]
-                    [_::TITLE]Titulo de la tabla FullList2[#]
-                    [_::CONTENT][*:::model/web/objects/Site/html/views/FullList2][#][#]
-                    [_::FOOTER]Pie de pagina de la tabla fulllist2[#]
-                [#]              
-
-            [#]
+        
+        [*:COMPONENTS/CARD]
+            [_:TITLE]Titulo de la tabla FullList2[#]
+            [_:CONTENT][*::model/web/objects/Site/html/views/FullList2][#][#]
+            [_:FOOTER]Pie de pagina de la tabla fulllist2[#]
         [#]
+
     [#]   
 [#]
