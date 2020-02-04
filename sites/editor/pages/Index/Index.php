f06fd063f6d0226174c4299521ba5c441f66847a
frodriguezsmartclip <frodriguez@smartclip.com>
2020-02-04 16:04:46 +0100

    2 tipos de layouts: vertical y horizontal, toolbar y menus. Cambiando los .wid en sites dentro de LAYOUT

--- sites/editor/pages/Index/Index.php
+++ sites/editor/pages/Index/Index.php
@@ -1,4 +1,4 @@
-[*PAGE/EDITOR]
+[*PAGE/INDEX]
     [_TITLE]Index SECCION - v.1.0 beta Smartclip[#]
 
     [_CONTENT]
@@ -9,25 +9,6 @@
             [_::CONTENT]
                 [*:/model/web/objects/Page/html/forms/AddAction][#]
             [#]
-        [#]
-
-
-        [*:COMPONENTS/CARD]
-            [_:CONTENT]Ejemplo de card SIN titulo y sin pie de pagina[#]
-            [_:FOOTER]Pie de pagina de la tabla fulllist2[#]
-            [_:CONTENT]Contenido de la card[#]
-        [#]
-
-        [*:COMPONENTS/CARD]
-            [_:TITLE]Titulo ejemplo iframe video[#]
-            [_:FOOTER]footer iframe[#]
-            [_:CONTENT]<iframe width="560" height="315" src="https://www.youtube.com/embed/6M5VXKLf4D4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>[#]
-        [#]
-
-        [*:COMPONENTS/CARD]
-            [_:TITLE]Titulo ejemplo iframe grafica[#]
-            [_:FOOTER]footer grafica[#]
-            [_:CONTENT]<div class="powr-graph" id="75c39f53_1579705574"></div><script src="https://www.powr.io/powr.js?platform=iframe#"></script>[#]
         [#]
 
     [#]
