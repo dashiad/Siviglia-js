0aeeba195b3ee88fc9dad6f840011333cb709dcd
frodriguezsmartclip <frodriguez@smartclip.com>
2020-02-05 17:59:27 +0100

    Nueva estructura widgets + eliminada antigua + AddAction & EditAction en Site.php y Page.php.  En Index.php avisa que funciona mal la parte de EditAction.

--- sites/editor/pages/Index/Index.php
+++ sites/editor/pages/Index/Index.php
@@ -3,11 +3,25 @@
 
     [_CONTENT]
 
-        [*::COMPONENTS/CARD]
-            [_::TITLE]Tareas en curso[#]
-            [_::FOOTER]Hay xx tareas en ejecución[#]
-            [_::CONTENT]
-                [*:/model/web/objects/Page/html/forms/AddAction][#]
+        [*:BEHAVIOR/CARD]
+            [_:TITLE]Titulo index.php[#]            
+            [_:CONTENT]                
+                [*::ELEMENT/CONTENT/PARRAFO]
+                    1. Pincha en un site del menu de navegacion. 
+                [#]
+                [*::ELEMENT/CONTENT/PARRAFO]
+                    2. Tabla de AddAction_testing.wid
+                [#]
+                [*::ELEMENT/CONTENT/PARRAFO]
+                    3. Si pinchamos en una pagina del FullList2, me da error porque llama a 
+
+                    model/web/objects/Site/html/forms/EditAction
+
+                    o 
+                    /TESTING_wids/EditAction_testing
+
+                    ambos widgets me petan :(
+                [#]   
             [#]
         [#]
 
--- sites/editor/pages/Page/Page.php
+++ sites/editor/pages/Page/Page.php
@@ -1,27 +1,23 @@
-[*PAGE/EDITOR]
+[*PAGE/PAGE]
     [_TITLE]PAGE SECCION - v.1.0 beta Smartclip[#]
 
     [_CONTENT]
 
-        [*:COMPONENTS/CARD]
-            [_:TITLE]Componente VENTANA_MODAL[#]
-            [_:COLLAPSE][_*][#]
+        [*:BEHAVIOR/CARD]
+            [_:TITLE]Editando page [#]
             [_:CONTENT]
-                [*::TAGS/P]Ejemplo de componente modal con tag p para poner parrafos antes del modal[#]
-                [*::COMPONENTS/MODAL/VENTANA_MODAL]
-                    [_::NOMBRE_BOTON_MODAL]Lanzar ventana-modal[#]
-                    [_::TITULO_MODAL]Ejemplo titulo modal[#]
-                    [_::COLOR_TITULO]white[#]
-                    [_::COLOR_CABECERA]success[#]
-                    [_::TAMANO]xl[#]
-                    [_::CENTRADO][_*][#]
-                    [_::CONTENIDO_MODAL]
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,                                                
-                    [#]
-                [#]
-
+                [*::/TESTING_wids/EditAction_testing][#]
             [#]
-        [#]
+        [#]       
         
     [#]   
 [#]
+
+<!-- esto me da error 
+
+[*::model/web/objects/Site/html/forms/EditAction][#]
+
+Y esta llamada al widget tambien me peta:
+[*::/TESTING_wids/EditAction_testing][#]
+
+-->
