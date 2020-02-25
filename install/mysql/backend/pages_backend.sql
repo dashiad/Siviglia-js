ae78e92a5f9588893ef12365d82374d74a45719a
Andrés Purriños <apurrinos@smartclip.com>
2020-02-25 12:27:50 +0000

    Añadidos insert sql para páginas de backend

---
+++ install/mysql/backend/pages_backend.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM adtopy.page where id_page>19
+LIMIT 0, 1000
+
+-- Date: 2020-02-25 13:21
+*/
+INSERT INTO `page` (`id_page`,`tag`,`id_site`,`name`,`date_add`,`date_modified`,`id_type`,`isPrivate`,`path`,`title`,`tags`,`description`,`role`) VALUES (20,'index',5,'Index',NULL,NULL,1,0,'index','titulo','tags','desc',NULL);
+INSERT INTO `page` (`id_page`,`tag`,`id_site`,`name`,`date_add`,`date_modified`,`id_type`,`isPrivate`,`path`,`title`,`tags`,`description`,`role`) VALUES (21,'error',5,'Error',NULL,NULL,1,0,'error','Error',NULL,NULL,NULL);
+INSERT INTO `page` (`id_page`,`tag`,`id_site`,`name`,`date_add`,`date_modified`,`id_type`,`isPrivate`,`path`,`title`,`tags`,`description`,`role`) VALUES (22,'Job',5,'Job',NULL,NULL,1,0,'Job','Job',NULL,NULL,NULL);
