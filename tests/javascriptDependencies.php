93ebb75f7ff36566178dc2d2fd0ba2c7f0e81c84
Daniel Garcia <dgarcia@smartclip.com>
2022-02-23 11:29:31 +0100

    Extrae el funcionamiento de los test a un archivo
    modified:   tests/testLoader.php
    new file:   tests/testScripts.js

--- tests/javascriptDependencies.php
+++ tests/javascriptDependencies.php
@@ -13,12 +13,3 @@
 <script src="http://statics.adtopy.com/packages/jqwidgets/globalization/globalize.js"></script>
 
 <script src="/reflection/js/WampServer.js"></script>
-
-<link rel='stylesheet prefetch' href='http://statics.adtopy.com/node-modules/font-awesome/css/font-awesome.css'>
-<link rel="stylesheet" type="text/css" href="/reflection/css/style.css">
-<link rel="stylesheet" type="text/css" href="http://statics.adtopy.com/packages/Siviglia/jQuery/css/JqxWidgets.css">
-<link rel="stylesheet" type="text/css" href="http://statics.adtopy.com/packages/jqwidgets/styles/jqx.base.css">
-<link rel="stylesheet" type="text/css" href="http://statics.adtopy.com/packages/Siviglia/jQuery/css/jqx.adtopy-dev.css">
-<link rel="stylesheet" href="/backend/css/style.css">
-<link rel="stylesheet" type="text/css" href="testStyles.css">
-
