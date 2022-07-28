9d525fe9497fddfae50f87acae2d83539242272b
Daniel Garcia <dgarcia@smartclip.com>
2022-07-28 13:13:48 +0200

    Modifica los tests de UI para trabajar con path
    Se modifica testEngine para generar el path del archivo de test del
    query parameter path. Ahora el title de cada test tiene que ir dentro
    del propio archivo de test.

--- ui/tests/stubs/js/testEngine.js
+++ ui/tests/stubs/js/testEngine.js
@@ -1,8 +1,7 @@
 var urlParams = new URLSearchParams(window.location.search);
-if (urlParams.has('name')) {
+if (urlParams.has('path')) {
   var testLoader = document.createElement('script')
-  testLoader.src = 'http://statics.adtopy.com/packages/Siviglia/ui/tests/' + urlParams.get('name') + '.js'
-  document.getElementsByTagName('title')[0].innerHTML = urlParams.get('name')
+  testLoader.src = 'http://statics.adtopy.com/packages/Siviglia/ui/tests/' + urlParams.get('path') + '.js'
   document.body.appendChild(testLoader)
 }
 var DEVELOP_MODE;
