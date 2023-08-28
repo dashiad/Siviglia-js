4de6dc9cbc7628717c13f06dce087d095625528a
Daniel García <daniel.garcia@wemass.com>
2023-08-28 11:58:54 +0200

    Modify test selection and execution

--- ui/tests/dependencies/js/testEngine.js
+++ ui/tests/dependencies/js/testEngine.js
@@ -1,26 +1,81 @@
-var urlParams = new URLSearchParams(window.location.search);
+const urlParams = new URLSearchParams(window.location.search);
+const TEST_DESTROY = urlParams.get('test') || true
+const TEST_FOLDER_PATH = '/packages/Siviglia/ui/tests/'
 
+let testPath
+let isSingleTest = false
+let pathParameter
 if (urlParams.has('path')) {
-  testFilePath = '/packages/Siviglia/ui/tests/' + urlParams.get('path')
-  // Siviglia.require(['/packages/Siviglia/ui/tests/' + urlParams.get('path')], true, false)
-} else {
-  console.log('No se ha indicado un path para el test')
+  pathParameter = urlParams.get('path')
+  testPath = TEST_FOLDER_PATH + pathParameter
+  const splitPath = pathParameter.split('/')
+  const lastPathElement = splitPath.at(-1)
+  if (hasKnownExtension(lastPathElement)) {
+    isSingleTest = true
+  }
 }
+var testStack = [];
 
-var DEVELOP_MODE;
-if (!urlParams.has("test")) {
-  //DEVELOP_MODE=47;    // Specific test number
-  //DEVELOP_MODE=0;     // All tests
-  //DEVELOP_MODE=(-1);  // Latest test
-  DEVELOP_MODE = (-1);
-} else {
-  DEVELOP_MODE = parseInt(urlParams.get("test"));
+function hasKnownExtension(str) {
+  const knownExtensions = ['js', 'css', 'html', 'php']
+  if (str.includes('.')) {
+    for (const extension of knownExtensions) {
+      if (str.split('.').pop() === extension) {
+        return true
+      }
+    }
+  }
+  return false
 }
-
-var TEST_DESTROY = true;
 
 hljs.initHighlightingOnLoad();
 
+
+function getStringBetween(str, start, end) {
+  const result = str.match(new RegExp(start + "(.*)" + end, 's'));
+
+  return result[1];
+}
+
+function fetchTestContent(path, index) {
+  var testPromise = $.Deferred();
+  /* Parece que no hace falta incluir el path completo del test
+  *  Se deja el código por si en un futuro se quiere cambiar
+  * */
+  /*var uiTestPath = '/packages/Siviglia/ui/tests/'
+  var url = Siviglia.config.staticsUrl + uiTestPath + path
+  $.get(url).then(function(res) {*/
+  $.get(path).then(function (res) {
+    testStack[index].template = getStringBetween(res, '<!-- templateInit -->', '<!-- templateEnd -->').replace(/(\r\n|\n|\r)/gm, "");
+    testStack[index].view = getStringBetween(res, '<!-- viewInit -->', '<!-- viewEnd -->').replace(/(\r\n|\n|\r)/gm, "");
+    testStack[index].callback = Function(getStringBetween(res, '//codeInit', '//codeEnd'))
+    testPromise.resolve()
+  })
+
+  return testPromise
+}
+
+function createTestStack(listToBuild) {
+  const stackPromise = $.Deferred()
+  const testPromises = []
+  let testIndex = 0
+  for (const test of listToBuild) {
+    testStack.push({
+      number: testIndex + 1,
+      name: test.name,
+      doc: test.doc,
+    })
+    testPromises.push(fetchTestContent(test.path, testIndex))
+
+    testIndex++
+  }
+
+  $.when.apply($, testPromises).done(function () {
+    stackPromise.resolve()
+  })
+
+  return stackPromise
+}
 
 function countListeners() {
   var s = 0;
@@ -34,18 +89,6 @@ function countManagers() {
   for (var k in Siviglia.Dom.existingManagers)
     s++;
   return s;
-}
-
-function showResult(name, doc, template, view, result, callback, testNumber, exception) {
-  var className = "result resultError";
-  var message = "ERROR";
-  if (!exception) {
-    if (JSON.stringify(expected) === JSON.stringify(result)) {
-      className = "result resultOk";
-      message = "OK!";
-    }
-  } else
-    message = "ERROR:" + exception;
 }
 
 var formatHTML = function (code, stripWhiteSpaces, stripEmptyLines) {
@@ -118,7 +161,7 @@ function createHTMLTestStructure(test) {
   var classContainer = createHTMLElement('div', 'testCode', codeContainer)
   var classTitle = createHTMLElement('div', 'resultTitle', classContainer, 'Code')
   var classContent = createHTMLElement('div', 'resultCode', classContainer)
-  classContent.innerHTML = "<pre>" + hljs.highlightAuto(test.cb.toString()).value + "</pre>";
+  classContent.innerHTML = "<pre>" + hljs.highlightAuto(test.callback.toString()).value + "</pre>";
 
   var divClear = createHTMLElement('div', null, testContainer)
   divClear.style.clear = "both";
@@ -133,7 +176,7 @@ function createHTMLTestStructure(test) {
   var parser = new Siviglia.UI.HTMLParser();
   parser.parse($("#testCont" + test.number));
 
-  if (TEST_DESTROY === true && DEVELOP_MODE !== 0) {
+  if (TEST_DESTROY === true && isSingleTest) {
     var onClicked = function () {
       var nListeners = countListeners();
       var nManagers = countManagers();
@@ -164,27 +207,11 @@ function createHTMLTestStructure(test) {
   }
 }
 
-var testStack = [];
-
-/*// Test extractor
-var indexContent = `document.getElementsByTagName('title')[0].innerHTML = 'Tests expandos'\n\nvar promiseList = []\n\n`*/
-
-function checkTests(restart) {
-
-  /*// Test extractor
-  saveFileInUserStorage('index.js', indexContent, 'application/javascript')
-   */
-
-  if (restart !== false && DEVELOP_MODE !== 0) {
-    if (DEVELOP_MODE === -1)
-      testStack = [testStack.pop()];
-    else
-      testStack = [testStack[DEVELOP_MODE - 1]];
-  }
+function buildTests() {
   while (testStack.length > 0) {
     var currentTest = testStack.shift();
     try {
-      currentTest.cb.apply(null);
+      currentTest.callback.apply(null);
       createHTMLTestStructure(currentTest)
     } catch (error) {
       console.log('Falla el test número ' + currentTest.number + ' -> ' + currentTest.name)
@@ -193,72 +220,23 @@ function checkTests(restart) {
   }
 }
 
-var testNumber = 0;
-
-function runTest(name, doc, template, view, callback) {
-
-  /*// Test extractor
-  var templateContent = '<!-- templateInit -->\n'+template+ '\n<!-- templateEnd -->\n\n'
-  var viewContent = '<!-- viewInit -->\n'+view+ '\n<!-- viewEnd -->\n\n'
-  var codeContent = '<script>\n  //codeInit\n'+ getStringBetween(callback.toString(2),'{\n  ','}')+'  \n//codeEnd\n</script>'
-  var fileContent = templateContent+viewContent+codeContent
-  indexContent += `promiseList.push(addTestPromise(
-  '${name}',
-  '${doc}',
-  'path unsetted'
-))\n`
-  saveFileInUserStorage(name+'.html', fileContent, 'text/html')*/
-
-  testNumber++;
-  if (typeof expectedResult == "undefined")
-    expectedResult = true;
-
-  return testStack.push({
-    name: name,
-    doc: doc,
-    template: template,
-    view: view,
-    cb: callback,
-    number: testNumber
-  });
-}
-
-function saveFileInUserStorage(filename, data, type) {
-  const file = new Blob([data], {type: type})
-  const a = document.createElement('a')
-  const url = URL.createObjectURL(file)
-  a.href = url
-  a.download = filename
-  document.body.appendChild(a)
-  a.click()
-  setTimeout(function () {
-    document.body.removeChild(a)
-    window.URL.revokeObjectURL(url)
-  }, 0)
-}
-
-
-function addTestPromise(name, doc, path) {
-  var currentTestIndex = runTest(name, doc) - 1
-  var promise = $.Deferred();
-  /* Parece que no hace falta incluir el path completo del test
-  *  Se deja el código por si en un futuro se quiere cambiar
-  * */
-  /*var uiTestPath = '/packages/Siviglia/ui/tests/'
-  var url = Siviglia.config.staticsUrl + uiTestPath + path
-  $.get(url).then(function(res) {*/
-  $.get(path).then(function (res) {
-    testStack[currentTestIndex].template = getStringBetween(res, '<!-- templateInit -->', '<!-- templateEnd -->').replace(/(\r\n|\n|\r)/gm, "");
-    testStack[currentTestIndex].view = getStringBetween(res, '<!-- viewInit -->', '<!-- viewEnd -->').replace(/(\r\n|\n|\r)/gm, "");
-    testStack[currentTestIndex].cb = Function(getStringBetween(res, '//codeInit', '//codeEnd'))
-    promise.resolve()
-  })
-
-  return promise
-}
-
-function getStringBetween(str, start, end) {
-  const result = str.match(new RegExp(start + "(.*)" + end, 's'));
-
-  return result[1];
-}
+Siviglia.require(['dependencies/js/testsList.js'], true, false).then(function () {
+  let selectedTests = []
+  if (isSingleTest) {
+    selectedTests.push(testsList.find(function (test) {
+      return test.path === pathParameter
+    }))
+  } else if (urlParams.has('path')) {
+    for (const test of testsList) {
+      if (test.path.includes(pathParameter)) {
+        selectedTests.push(test)
+      }
+    }
+  } else {
+    selectedTests = testsList
+  }
+    createTestStack(selectedTests).then(function () {
+      buildTests()
+    })
+  }
+)
