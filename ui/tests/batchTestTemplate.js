5a586639e341c1e97bc9c8b781746c3f45140349
Daniel García <daniel.garcia@wemass.com>
2023-08-28 16:49:09 +0200

    Comment some lines in tests index

--- ui/tests/batchTestTemplate.js
+++ ui/tests/batchTestTemplate.js
@@ -4,12 +4,12 @@ document.getElementsByTagName('title')[0].innerHTML = 'Batch test template'
 var promiseList = []
 
 // Añadir los tests a la lista de la siguiente forma:
-promiseList.push(addTestPromise(
+promiseList.push(fetchTestContent(
   "Nombre de test 1",
   "documentación 1",
   'testTemplate.html'
 ))
-promiseList.push(addTestPromise(
+promiseList.push(fetchTestContent(
   "Nombre de test 2",
   "documentación 2",
   'testTemplate.html'
